671b3a2b38ca8c81ea5aedf215bab8218c89e513
who_th3_h3ll <elemanhillary@gmail.com>
2017-01-12 17:17:40 +0300

    Create consumer.java

---
+++ consumer.java
@@ -0,0 +1,12 @@
+consumer implements Runnable{
+factory fact;
+
+consumer(factory fact){
+this.fact = fact;
+new Thread(this,"Consumer Thread").start();
+}
+public void run(){
+while(true){
+fact.get();
+}
+}
